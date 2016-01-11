8cbc671970c7cac2d911538e891ca1f08dac6d69
wrxavex <wrxavex@gmail.com>
2016-01-12 01:18:38 +0800

    djangogirl test chapter 5

--- djangoznh_venv/mysite/mysite/urls.py
+++ djangoznh_venv/mysite/mysite/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from trips.views import hello_world
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello_world),
 ]
+
--- djangoznh_venv/mysite/trips/views.py
+++ djangoznh_venv/mysite/trips/views.py
@@ -1,3 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+def hello_world(request):
+    return render(request, 'hello_world.html',{
+        'current_time': datetime.now(),
+    })
+
